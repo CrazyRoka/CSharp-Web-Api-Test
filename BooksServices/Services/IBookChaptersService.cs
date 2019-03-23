4bfb1c52c5200038707acf92ee7733a0958d9709
Rostyslav <RokaRostuk@gmail.com>
2019-03-23 23:44:43 +0200

    Add project files.

---
+++ BooksServices/Services/IBookChaptersService.cs
@@ -0,0 +1,17 @@
+﻿using BooksServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BooksServices.Services
+{
+    public interface IBookChaptersService
+    {
+        void Add(BookChapter bookChapter);
+        void AddRange(IEnumerable<BookChapter> chapters);
+        IEnumerable<BookChapter> GetAll();
+        BookChapter Find(Guid id);
+        BookChapter Remove(Guid id);
+        void Update(BookChapter bookChapter);
+    }
+}
